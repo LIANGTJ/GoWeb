55314ab761ef9d41b3e53fc31257c61c003caccd
LIANGTJ <liangtjsky@163.com>
2017-11-14 20:36:40 +0800

    use negoni

--- main.go
+++ main.go
@@ -2,28 +2,20 @@ package main
 
 import (
 	"fmt"
-	"log"
+	// "github.com/gorilla/mux"
+	"github.com/urfave/negroni"
 	"net/http"
-	"strings"
 )
 
-func sayhelloName(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()       //解析参数，默认是不会解析的
-	fmt.Println(r.Form) //这些信息是输出到服务器端的打印信息
-	fmt.Println("path", r.URL.Path)
-	fmt.Println("scheme", r.URL.Scheme)
-	fmt.Println(r.Form["url_long"])
-	for k, v := range r.Form {
-		fmt.Println("key:", k)
-		fmt.Println("val:", strings.Join(v, ""))
-	}
-	fmt.Fprintf(w, "Hello astaxie!") //这个写入到w的是输出到客户端的
-}
-
 func main() {
-	http.HandleFunc("/", sayhelloName)       //设置访问的路由
-	err := http.ListenAndServe(":9090", nil) //设置监听的端口
-	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
-	}
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		fmt.Fprintf(w, "Welcome to the home page!")
+	})
+
+	fmt.Println(b, b["name"])
+	n := negroni.Classic()
+	n.UseHandler(mux)
+	n.Run(":9090")
+
 }
